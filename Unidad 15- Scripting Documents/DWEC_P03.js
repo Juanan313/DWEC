6a367e96ede115e165b67dce7c41fdc7a2206d4b
Juan Antonio Pujals Echeverría <jpujals@quonext-tur.com>
2019-01-28 12:29:37 +0100

    practica 2

--- Unidad 15- Scripting Documents/DWEC_P03.js
+++ Unidad 15- Scripting Documents/DWEC_P03.js
@@ -1,3 +1,39 @@
 window.onload = function () {
-
+    remplazarHref('http', 'https');
+    marcarImportantes();
   };
+
+  var body = document.body;
+  var enlaces = body.querySelectorAll('a');
+
+ function remplazarHref(texto, remplazo) {
+
+     enlaces.forEach( enlace => {
+         enlace.href = enlace.href.replace(texto, remplazo);
+     });    
+ }
+
+ function resaltarImportante(elemento) {
+    elemento.setAttribute('class', 'resaltar');
+ }
+
+ function marcarNormal(elemento) {
+    elemento.setAttribute('class', 'normal');
+ }
+
+ function marcarImportantes() {
+     var indiceImportante = 0;
+     enlaces.forEach( enlace => {
+        if (enlace.getAttribute('class') && enlace.getAttribute('class').includes('importante')) {
+            resaltarImportante(enlace);
+            nameImportant(enlace, 'importante'+indiceImportante);
+            indiceImportante ++;
+        } else {
+            marcarNormal(enlace);
+        }
+     })
+ }
+
+ function nameImportant(enlace, nombre) {
+     enlace.setAttribute('name', nombre);
+ }
