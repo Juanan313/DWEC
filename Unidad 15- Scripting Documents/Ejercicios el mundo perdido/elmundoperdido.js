7836da2c01318b6048f1d43e112bf1cb0bb00de2
Juan Antonio Pujals Echeverría <jpujals@quonext-tur.com>
2019-01-28 12:10:07 +0100

    Ejercicio1 acabado y refact

--- Unidad 15- Scripting Documents/Ejercicios el mundo perdido/elmundoperdido.js
+++ Unidad 15- Scripting Documents/Ejercicios el mundo perdido/elmundoperdido.js
@@ -1,65 +1,69 @@
 window.onload = function () {
+    body = document.body;
 
-    // obtener los datos para el ejercicio
-    var enlaces = contarElementos('a');
-    var parrafos = contarElementos('p');
-    var penultimoEnlace = enlaces[enlaces.length - 2];
-    var ultimoEnlace = enlaces[enlaces.length - 1];
-    var enlacesAJurassicPark = contarEnlacesA('http://www.jurassicpark.com/', enlaces);
-    var numeroEnlacesPar = contarElementosHijo(parrafos, 'a');
+    getInfo();
+    var info = prepareInfo();
+    pintarInfo(info);
+}
 
-    // añadir los daots al DOM
-    var body = document.body;
+
+// obtener los datos para el ejercicio
+function getInfo() {
+    enlaces = contarElementos('a');
+    parrafos = contarElementos('p');
+    penultimoEnlace = enlaces[enlaces.length - 2];
+    ultimoEnlace = enlaces[enlaces.length - 1];
+    enlacesAJurassicPark = contarEnlacesA('http://www.jurassicpark.com/', enlaces);
+    numeroEnlacesPar = contarElementosHijo(parrafos, 'a');
+
+}
+// añadir los daots al DOM
+function pintarInfo (info) {
     var dataDiv = document.createElement('div');
     dataDiv.id = "dataDiv";
-
     var list = document.createElement('ul');
-    var li1 = document.createElement('li');
-    li1.innerHTML = "Número de enlaces: "+enlaces.length;
-    list.appendChild(li1);
-
-    var li2 = document.createElement('li');
-    li2.innerHTML = "Número de parrafos: "+ parrafos.length;
-    list.appendChild(li2);
-
-    var li3 = document.createElement('li');
-    li3.innerHTML = "Penúñtimo enlace: "+penultimoEnlace;
-    list.appendChild(li3);
-
-    var li4 = document.createElement('li');
-    li4.innerHTML = "Último enlace: "+ultimoEnlace;
-    list.appendChild(li4);
-
-    var li5 = document.createElement('li');
-    li5.innerHTML = "Número de enlaces a http://jurassicpark.com : "+enlacesAJurassicPark;
-    list.appendChild(li5);
+    info.forEach( text => {
+        var li = document.createElement('li');
+        li.innerHTML = text;
+        list.appendChild(li);
+    })
 
     dataDiv.appendChild(list);
     body.insertBefore(dataDiv, body.firstChild);
+}
 
+function contarElementos(tag) {
+    return document.querySelectorAll(tag);
+}
 
-    function contarElementos(tag) {
-        return document.querySelectorAll(tag);
-    }
+function contarEnlacesA(url, enlaces) {
+    let numEnlaces = 0;
+    enlaces.forEach( enlace => {
+        if (enlace.href == url) {
+            numEnlaces += 1;
+        } 
+    })
+    return numEnlaces;
+}
 
-    function contarEnlacesA(url, enlaces) {
-        let numEnlaces = 0;
-        enlaces.forEach( enlace => {
-            if (enlace.href == url) {
-                numEnlaces += 1;
-            } 
-        })
-        return numEnlaces;
-    }
+function contarElementosHijo (elementos, tag) {
+    var numeroHijos = 0;
+    elementos.forEach( elemento => {
+        numeroHijos += elemento.getElementsByTagName(tag).length;
+    });
+    console.log(numeroHijos);
+    return numeroHijos;
+}
 
-    function contarElementosHijo (elementos, tag) {
-        
-        var numeroHijos = 0;
-        elementos.forEach( elemento => {
-            console.log(elemento);
-        })
+function prepareInfo () {
+    let info = [];
 
-        return numeroHijos;
-    }
-    
+    info.push("Número de enlaces: "+enlaces.length);
+    info.push("Número de parrafos: "+ parrafos.length);
+    info.push("Penúñtimo enlace: "+penultimoEnlace);
+    info.push("Último enlace: "+ultimoEnlace);
+    info.push("Número de enlaces a http://jurassicpark.com : "+enlacesAJurassicPark);
+    info.push("Número de enlaces en parrafos: "+numeroEnlacesPar);
+
+    return info;
 }
